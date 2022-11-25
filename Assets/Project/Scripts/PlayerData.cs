e0f48f985fcc9f8a50bab4d2c961c6e1ca2a7708
Abraxas170600 <abraxassabaoth666@gmail.com>
2022-11-25 16:45:18 -0500

    PlayerData script added

---
+++ Assets/Project/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float movementVelocity;
+    public float jumpHeight;
+    public float jetpackRechargeRate;
+    public float fuelAmount;
+    public float boostStrength;
+}
