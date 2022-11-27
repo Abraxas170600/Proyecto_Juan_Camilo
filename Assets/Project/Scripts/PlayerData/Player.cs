8a1f93abf0e261b32a23f119842f332fbab14d93
Abraxas170600 <abraxassabaoth666@gmail.com>
2022-11-26 22:48:37 -0500

    Player scriptable object added

---
+++ Assets/Project/Scripts/PlayerData/Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Player")]
+public class Player : ScriptableObject
+{
+    public PlayerData _playerData;
+}
