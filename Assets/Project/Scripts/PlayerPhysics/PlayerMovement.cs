bb6a1128f18a699038cd3ec3b65c7fd953732112
Abraxas170600 <abraxassabaoth666@gmail.com>
2022-11-27 12:48:12 -0500

    PlayerMovement script fixed

--- Assets/Project/Scripts/PlayerPhysics/PlayerMovement.cs
+++ Assets/Project/Scripts/PlayerPhysics/PlayerMovement.cs
@@ -4,23 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float _horizontal;
-    private float _vertical;
-    private bool _isJump = false;
-
-    void Update()
+    public void UpdateMovement(float speed, Rigidbody rigidbody, Transform camPosition, float horizontal, float vertical)
     {
-        ProcessInputs();
-    }
-    private void ProcessInputs()
-    {
-        _horizontal = Input.GetAxis("Horizontal");
-        _vertical = Input.GetAxis("Vertical");
-        _isJump = Input.GetButtonDown("Jump");
-    }
-    public void UpdateMovement(float speed, Rigidbody rigidbody, Transform camPosition)
-    {
-        Vector3 direction = new Vector3(_horizontal, 0f, _vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camPosition.eulerAngles.y;
@@ -28,9 +14,9 @@ public class PlayerMovement : MonoBehaviour
             rigidbody.AddForce(moveDir.normalized * speed);
         }
     }
-    public void Jump(bool isGrounded, float jumpForce, Rigidbody rigidbody)
+    public void Jump(bool isGrounded, float jumpForce, Rigidbody rigidbody, bool isJump)
     {
-        if (_isJump && isGrounded)
+        if (isJump && isGrounded)
         {
             rigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
         }
