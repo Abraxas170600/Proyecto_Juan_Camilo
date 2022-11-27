a0086e2d3e8f78045f6d7643de47367e63918bdf
Abraxas170600 <abraxassabaoth666@gmail.com>
2022-11-27 12:47:31 -0500

    JetpackJump script added

---
+++ Assets/Project/Scripts/PlayerPhysics/JetpackJump.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetpackJump : MonoBehaviour
+{
+    private float fuel;
+    public void Initialize(float fuelAmount)
+    {
+        fuel = fuelAmount;
+    }
+    public void Jetpack(bool isGrounded, Rigidbody rigidbody, float rechargeRate, float fuelAmount, float maxFuel, float boostStrength, bool jetpack)
+    {
+        if (jetpack && fuel > 0f)
+        {
+            rigidbody.AddForce(new Vector3(0, boostStrength, 0), ForceMode.Impulse);
+            fuel -= Time.deltaTime;
+        }
+        else if (jetpack == false && isGrounded && fuel < maxFuel)
+        {
+            fuel += rechargeRate * Time.deltaTime;
+        }
+    }
+}
