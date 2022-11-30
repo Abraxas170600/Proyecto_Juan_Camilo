2e92a55ec66077b2051c7e1938fe251b74038fe5
Abraxas170600 <abraxassabaoth666@gmail.com>
2022-11-30 11:50:11 -0500

    ProyectSettings fixed

--- Assets/Project/Scripts/ProjectSettings/ProjectSettings.cs
+++ Assets/Project/Scripts/ProjectSettings/ProjectSettings.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class ProjectSettings : MonoBehaviour
 {
+    //public CameraSettings _cameraSettings;
+    public static ProjectSettings _projectSettings;
     private void Awake()
     {
+        _projectSettings = this;
         Application.targetFrameRate = 60;
-        Cursor.lockState = CursorLockMode.Locked;
+        StateGame(1);
     }
+    public void StateGame(int state)
+    {
+        if (state <= 0)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            //_cameraSettings.StateCamera(true);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            //_cameraSettings.StateCamera(false);
+        }
+    }
+    public void QuitGame() => Application.Quit();
 }
