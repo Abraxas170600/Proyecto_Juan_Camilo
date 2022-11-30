6a959bc8f0400aa9f2394f008193fb90de8b7170
Abraxas170600 <abraxassabaoth666@gmail.com>
2022-11-30 12:06:43 -0500

    GameEvents script added

---
+++ Assets/Project/Scripts/ProjectSettings/GameEvents.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents _gameEvents;
+    private void Awake()
+    {
+        _gameEvents = this;
+    }
+    public event Action ScoreDetected;
+    public void Detected()
+    {
+        if(ScoreDetected != null)
+            ScoreDetected();
+    }
+    public event Action AddPoint;
+    public void Add()
+    {
+        if (AddPoint != null)
+            AddPoint();
+    }
+    public event Action AddScoreText;
+    public void AddOnText()
+    {
+        if (AddScoreText != null)
+            AddScoreText();
+    }
+}
